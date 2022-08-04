935479a76c7b1c8a1e81c75980e157cb57cc2811
Tom Rossner <tomrossner2@gmail.com>
2022-08-04 12:49:41 +0300

    Changed hamburger menu

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-function menuToggle(){
-    let hamburger = document.getElementById("hamburger");
-    let hamburgerClose = document.getElementById("hamburger-close");
-    let navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("open");
-    if(hamburger.style.display !== "none"){
-        hamburger.style.display = "none";
-        hamburgerClose.style.display = "initial";
-    }else{
-        hamburger.style.display = "initial";
-        hamburgerClose.style.display = "none";
-    }
-}
+// function menuToggle(){
+//     let hamburger = document.getElementById("hamburger");
+//     let hamburgerClose = document.getElementById("hamburger-close");
+//     const navLinks = document.getElementById("navLinks");
+//     navLinks.classList.toggle("open");
+//     if(hamburger.style.display !== "none"){
+//         hamburger.style.display = "none";
+//         hamburgerClose.style.display = "initial";
+//     }else{
+//         hamburger.style.display = "initial";
+//         hamburgerClose.style.display = "none";
+//     }
+// }
 
+const menu = document.querySelector(".menu");
+
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("x");
+    navLinks.classList.toggle("open");
+})
 
     const chevron = document.querySelectorAll('.bi.bi-chevron-right');
     const cards = document.querySelectorAll('.cards');
